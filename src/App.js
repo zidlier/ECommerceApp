1764307f12e0d5283d93382059431ac7220c368f
aylsworth <patrick@skyciv.com>
2022-10-12 23:46:12 +0800

    Initial commit. Added routers, links etc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <div>
+        <header>
+          <Link to='/'>Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>} />
+            <Route path="/product/:slug" element={<ProductScreen/>} />
+            
+            
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d9bb3f55-801e-4cb0-963e-9ea2c01dce46/sportswear-max90-t-shirt-tdVGGv.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            reting: 4.5,
+            numReviews: 10,
+            description: 'High Quality Shirt'
+        },
+        {
+            name: 'Nike Slim Pants',
+            slug: 'nike-slim-pants',
+            category: 'Pants',
+            image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/792ab50c-531c-415f-8eae-f5ad8ea2c295/dri-fit-challenger-mens-woven-running-pants-VwT2cP.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            reting: 4.5,
+            numReviews: 10,
+            description: 'High Quality Shirt'
+        }
+    ]
+}
+
+
+export default data;
